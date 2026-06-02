2fba4be089c3e77ebcee9463264387659107e8b1
HFandSiO2 <392377626@qq.com>
2026-06-02 21:58:10 +0800

    忘删了，嘻嘻

--- components/drivers/gpio_def/gpio_def.c
+++ components/drivers/gpio_def/gpio_def.c
@@ -5,8 +5,9 @@
 
 static const char *TAG = "GPIO_DEF";
 
-
 void gpio_init(void) {
-    
+    /* 各模块自行初始化自己的引脚（dht11, ssd1306, spi_bus 等），
+       此函数保留用于未来需要统一配置的引脚 */
+    ESP_LOGI(TAG, "GPIO system ready");
 }
 
